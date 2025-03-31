cc2b02c2949f06de287cc09711941919c1582061
Ahsm123 <10503096@ucn.dk>
2025-03-31 20:30:55 +0200

    Made pipeline stateless added simple mappingSerivce test for mappingService

---
+++ Test/Tests/TransformServices/MappingServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using ETL.Domain.Model;
+using Transform.Services;
+
+namespace Test.Tests.TransformServices;
+
+public class MappingServiceTests
+{
+    private readonly MappingService _mappingService = new();
+
+    [Fact]
+    public void Apply_MapsFieldAndRemovesOriginal()
+    {
+        // Arrange 
+        var item = new Dictionary<string, object> { { "account_id", 123 }, { "status", "Accepted" } };
+        var mappings = new List<FieldMapping>
+            {
+                new() { SourceField = "account_id", TargetField = "id" }
+            };
+
+        // Act 
+        var result = _mappingService.Apply(item, mappings);
+
+        // Assert 
+        Assert.False(result.ContainsKey("account_id"));
+        Assert.True(result.ContainsKey("id"));
+        Assert.Equal(123, result["id"]);
+        Assert.Equal("Accepted", result["status"]);
+    }
+}
--- Transform/Services/TransformPipeline.cs
+++ Transform/Services/TransformPipeline.cs
@@ -5,18 +5,26 @@ namespace Transform.Services;
 
 public class TransformPipeline : ITransformPipeline
 {
+    private readonly FilterService _filterService;
     private readonly MappingService _mappingService;
 
-    public TransformPipeline(MappingService mappingService)
+    public TransformPipeline(
+        MappingService mappingService,
+        FilterService filterService)
     {
         _mappingService = mappingService;
+        _filterService = filterService;
     }
 
     public TransformPayload Execute(ExtractedPayload input)
     {
-        var mappedData = _mappingService.Apply(input.Data, input.Transform?.Mappings ?? new());
+        if (input.Transform?.Filters != null && !_filterService.ShouldInclude(input.Data, input.Transform.Filters))
+        {
+            throw new InvalidOperationException($"Payload {input.Id} did not pass filters");
+        }
 
-        var json = JsonSerializer.Serialize(mappedData);
+        var mapped = _mappingService.Apply(input.Data, input.Transform?.Mappings ?? new());
+        var json = JsonSerializer.Serialize(mapped);
         var jsonElement = JsonDocument.Parse(json).RootElement.Clone();
 
         return new TransformPayload
