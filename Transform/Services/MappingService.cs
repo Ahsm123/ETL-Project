1d2919e20eacc40eeef127c0e510e2ad67f48f55
Ahsm123 <10503096@ucn.dk>
2025-03-31 16:50:21 +0200

    Kan consume fra rawData og mappe. Kan produce til processedData

---
+++ Transform/Services/MappingService.cs
@@ -0,0 +1,24 @@
+﻿using ETL.Domain.Model;
+
+namespace Transform.Services;
+
+// MappingService.cs
+public class MappingService
+{
+    public Dictionary<string, object?> Apply(Dictionary<string, object> item, List<FieldMapping> mappings)
+    {
+        var result = new Dictionary<string, object?>(item);
+
+        foreach (var mapping in mappings)
+        {
+            if (item.TryGetValue(mapping.SourceField, out var value))
+            {
+                result.Remove(mapping.SourceField);
+                result[mapping.TargetField] = value;
+            }
+        }
+
+        return result;
+    }
+}
+
---
+++ Transform/Services/TransformPipeline.cs
@@ -0,0 +1,31 @@
+﻿using ETL.Domain.Model.DTOs;
+using System.Text.Json;
+
+namespace Transform.Services;
+
+public class TransformPipeline : ITransformPipeline
+{
+    private readonly MappingService _mappingService;
+
+    public TransformPipeline(MappingService mappingService)
+    {
+        _mappingService = mappingService;
+    }
+
+    public TransformPayload Execute(ExtractedPayload input)
+    {
+        var mappedData = _mappingService.Apply(input.Data, input.Transform?.Mappings ?? new());
+
+        var json = JsonSerializer.Serialize(mappedData);
+        var jsonElement = JsonDocument.Parse(json).RootElement.Clone();
+
+        return new TransformPayload
+        {
+            Id = input.Id,
+            SourceType = input.SourceType,
+            Load = input.Load,
+            Data = jsonElement
+        };
+    }
+
+}
