637f5a3c7e0e817a2e4872b718bc73c6b128f47b
ThomasUCN <10503119@ucn.dk>
2025-04-11 09:07:06 +0200

    Mysqlquerybuilder select test

--- Test/ExtractAPITest/DataSourceProviderTest/MySQLQueryBuilderTest.cs
+++ Test/ExtractAPITest/DataSourceProviderTest/MySQLQueryBuilderTest.cs
@@ -44,6 +44,78 @@ namespace Test.ExstractAPITest.DataSourceProviderTest
             Assert.Equal("1000", parameters.Get<string>("@p0"));
             Assert.Equal("approved", parameters.Get<object>("@p1"));
         }
-    }
 
+        [Fact]
+        public void BuildSelectQuery_GeneratesValidSQLWithoutFilter()
+        {
+            //arrange
+            var queryBuilder = new MySQLQueryBuilder();
+            var sourceInfo = new MySQLSourceInfo
+            {
+                TargetTable = "approved_highvalue_payments"
+            };
+            var fields = new List<string> { "account_id", "cost", "status" };
+            //Act
+            var (sql, parameters) = queryBuilder.BuildSelectQuery(sourceInfo, fields, null);
+            //Assert
+            var expectedSql =
+                "SELECT `account_id`, `cost`, `status` FROM `approved_highvalue_payments`;";
+            Assert.Equal(expectedSql, sql.Trim());
+        }
+        [Fact]
+        public void BuildSelectQuery_GeneratesValidSQLWithEmptyFields()
+        {
+            //arrange
+            var queryBuilder = new MySQLQueryBuilder();
+            var sourceInfo = new MySQLSourceInfo
+            {
+                TargetTable = "approved_highvalue_payments"
+            };
+            var fields = new List<string>();
+            //Act
+            var (sql, parameters) = queryBuilder.BuildSelectQuery(sourceInfo, fields, null);
+            //Assert
+            var expectedSql =
+                "SELECT * FROM `approved_highvalue_payments`;";
+            Assert.Equal(expectedSql, sql.Trim());
+        }
+        [Fact]
+        public void BuildSelectQuery_ThrowsExceptionWhenEmptyEmptyTargetTable()
+        {
+            //Arrange
+            var queryBuilder = new MySQLQueryBuilder();
+            var sourceInfo = new MySQLSourceInfo
+            {
+                TargetTable = ""
+            };
+            var fields = new List<string> { "account_id", "cost", "status" };
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentException>(() =>
+                queryBuilder.BuildSelectQuery(sourceInfo, fields, null));
+
+            Assert.Equal("Target table is required", ex.Message);
+
+        }
+        [Fact]
+        public void BuildSelectQuery_WhenFilterRuleIsMissingAttribute_ThrowsException()
+        {
+            // Arrange
+            var queryBuilder = new MySQLQueryBuilder();
+            var sourceInfo = new MySQLSourceInfo
+            {
+                TargetTable = "approved_highvalue_payments"
+            };
+            var fields = new List<string> { "account_id", "cost", "status" };
+            var filters = new List<FilterRule>
+            {
+                new FilterRule { Field = "cost", Operator = "", Value = "1000" }
+            };
+
+            // Act & Assert
+            var ex = Assert.Throws<ArgumentException>(() =>
+                queryBuilder.BuildSelectQuery(sourceInfo, fields, filters));
+
+            Assert.Equal("Unsupported operator ''", ex.Message);
+        }
+    }
 }
