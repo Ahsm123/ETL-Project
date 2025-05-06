102e614fbe6210c4f889428969eb8383732665b2
Ahsm123 <10503096@ucn.dk>
2025-05-06 19:04:21 +0200

    Move SQL Commands to the classes that uses them
    Delete ConnectionValidatorFactory. Wasnt being used, since there was duplicate logic in ConnectionValidatorResolver

--- ETLConfig.API/Services/Validators/MsSqlServerConnectionValidator.cs
+++ ETLConfig.API/Services/Validators/MsSqlServerConnectionValidator.cs
@@ -1,5 +1,4 @@
 ﻿using Dapper;
-using ETLConfig.API.Constants;
 using ETLConfig.API.Models.DTOs;
 using ETLConfig.API.Services.Interfaces;
 using Microsoft.Data.SqlClient;
@@ -8,6 +7,37 @@ namespace ETLConfig.API.Services.Validators;
 
 public class MsSqlConnectionValidator : IConnectionValidator
 {
+    private const string GetTablesQuery = @"
+        SELECT TABLE_NAME 
+        FROM INFORMATION_SCHEMA.TABLES 
+        WHERE TABLE_TYPE = 'BASE TABLE'";
+    private const string GetColumsQuery = @"
+        SELECT 
+        COLUMN_NAME AS ColumnName, 
+        DATA_TYPE AS DataType, 
+        CASE WHEN IS_NULLABLE = 'YES' THEN CAST(1 AS bit) ELSE CAST(0 AS bit) END AS IsNullable,
+        CHARACTER_MAXIMUM_LENGTH AS MaxLength,
+        COLUMNPROPERTY(OBJECT_ID(TABLE_NAME), COLUMN_NAME, 'IsIdentity') AS IsAutoIncrement
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_NAME = @table";
+    private const string GetPrimaryKeysQuery = @"
+        SELECT COLUMN_NAME 
+        FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE 
+        WHERE OBJECTPROPERTY(OBJECT_ID(CONSTRAINT_NAME), 'IsPrimaryKey') = 1 
+          AND TABLE_NAME = @table";
+    private const string GetForeignKeysQuery = @"
+        SELECT 
+    fkc.COLUMN_NAME AS [Column],
+    pk.TABLE_NAME AS ReferencedTable,
+    pkc.COLUMN_NAME AS ReferencedColumn
+FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc
+JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE fkc 
+    ON rc.CONSTRAINT_NAME = fkc.CONSTRAINT_NAME
+JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE pkc 
+    ON rc.UNIQUE_CONSTRAINT_NAME = pkc.CONSTRAINT_NAME
+JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS pk 
+    ON pk.CONSTRAINT_NAME = rc.UNIQUE_CONSTRAINT_NAME
+WHERE fkc.TABLE_NAME = @table";
     public async Task<bool> IsValidAsync(string connectionString)
     {
         try
@@ -28,18 +58,18 @@ public class MsSqlConnectionValidator : IConnectionValidator
         await connection.OpenAsync();
 
         var metadata = new DatabaseMetadata();
-        var tableNames = (await connection.QueryAsync<string>(SqlQueryConstants.MSSQL_GetTables)).ToList();
+        var tableNames = (await connection.QueryAsync<string>(GetTablesQuery)).ToList();
 
         foreach (var table in tableNames)
         {
             var columns = (await connection.QueryAsync<ColumnMetadata>(
-                SqlQueryConstants.MSSQL_GetColumns, new { table })).ToList();
+                GetColumsQuery, new { table })).ToList();
 
             var primaryKeys = (await connection.QueryAsync<string>(
-                SqlQueryConstants.MSSQL_GetPrimaryKeys, new { table })).ToList();
+                GetPrimaryKeysQuery, new { table })).ToList();
 
             var foreignKeys = (await connection.QueryAsync<ForeignKeyInfo>(
-                SqlQueryConstants.MSSQL_GetForeignKeys, new { table })).ToList();
+                GetForeignKeysQuery, new { table })).ToList();
 
             var tableMetadata = new TableMetadata
             {
--- ETLConfig.API/Services/Validators/MySqlServerConnectionValidator.cs
+++ ETLConfig.API/Services/Validators/MySqlServerConnectionValidator.cs
@@ -1,5 +1,4 @@
 ﻿using Dapper;
-using ETLConfig.API.Constants;
 using ETLConfig.API.Models.DTOs;
 using ETLConfig.API.Services.Interfaces;
 using MySql.Data.MySqlClient;
@@ -8,6 +7,27 @@ namespace ETLConfig.API.Services.Validators;
 
 public class MySqlServerConnectionValidator : IConnectionValidator
 {
+    private const string GetTablesQuery = "SHOW TABLES";
+    private const string GetColumnsQuery = @"
+    SELECT 
+        COLUMN_NAME AS ColumnName,
+        DATA_TYPE AS DataType,
+        CASE WHEN IS_NULLABLE = 'YES' THEN TRUE ELSE FALSE END AS IsNullable,
+        CHARACTER_MAXIMUM_LENGTH AS MaxLength,
+        CASE WHEN EXTRA LIKE '%auto_increment%' THEN TRUE ELSE FALSE END AS IsAutoIncrement
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = @table
+    AND TABLE_SCHEMA = DATABASE();";
+    private const string GetPrimaryKeysQuery = "SHOW KEYS FROM `{0}` WHERE Key_name = 'PRIMARY'";
+    private const string GetForeignKeysQuery = @"
+SELECT 
+    COLUMN_NAME AS `Column`,
+    REFERENCED_TABLE_NAME AS ReferencedTable,
+    REFERENCED_COLUMN_NAME AS ReferencedColumn
+FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+WHERE TABLE_NAME = '{0}' AND REFERENCED_TABLE_NAME IS NOT NULL";
+
+
     public async Task<bool> IsValidAsync(string connectionString)
     {
         try
@@ -28,24 +48,23 @@ public class MySqlServerConnectionValidator : IConnectionValidator
         await connection.OpenAsync();
 
         var metadata = new DatabaseMetadata();
-        var tableNames = (await connection.QueryAsync<string>(SqlQueryConstants.MYSQL_ShowTables)).ToList();
+        var tableNames = (await connection.QueryAsync<string>(GetTablesQuery)).ToList();
 
         foreach (var table in tableNames)
         {
             // Fetch column info
             var columns = (await connection.QueryAsync<ColumnMetadata>(
-    SqlQueryConstants.MYSQL_ShowColumns,
-    new { table })).ToList();
-
+         GetColumnsQuery,
+                new { table })).ToList();
 
             // Fetch primary keys
             var pkRaw = await connection.QueryAsync<dynamic>(
-                string.Format(SqlQueryConstants.MYSQL_ShowPrimaryKeys, table));
+                string.Format(GetPrimaryKeysQuery, table));
             var primaryKeys = pkRaw.Select(row => (string)row.Column_name).ToList();
 
             // Fetch foreign keys
             var foreignKeys = (await connection.QueryAsync<ForeignKeyInfo>(
-                string.Format(SqlQueryConstants.MYSQL_ShowForeignKeysAliased, table))
+                string.Format(GetForeignKeysQuery, table))
             ).ToList();
 
             // Assemble table metadata
@@ -65,7 +84,6 @@ public class MySqlServerConnectionValidator : IConnectionValidator
 
     private int? ParseMaxLengthFromType(string type)
     {
-        // Example type: varchar(255), decimal(10,2)
         var start = type.IndexOf('(');
         var end = type.IndexOf(')');
         if (start != -1 && end > start)
