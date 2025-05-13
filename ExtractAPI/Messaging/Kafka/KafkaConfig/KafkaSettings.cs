dd7bbda4bf12afaffd8ef4ee10be30bb55762294
Andershsm <10503096@ucn.dk>
2025-05-13 08:59:21 +0200

    Add Acks = 1 to all producers.
    Acks 0 in fire and forget, Acks 1 makes sure its written to atleast the Leader partition, avoids some data loss.

--- ExtractAPI/Messaging/Kafka/KafkaConfig/KafkaSettings.cs
+++ ExtractAPI/Messaging/Kafka/KafkaConfig/KafkaSettings.cs
@@ -4,6 +4,7 @@ public class KafkaSettings
 {
     public string BootstrapServers { get; set; }
     public string Topic { get; set; }
+    public string Acks { get; set; }
     public bool EnableIdempotence { get; set; }
     public int MessageSendMaxRetries { get; set; }
     public int RetryBackoffMs { get; set; }
