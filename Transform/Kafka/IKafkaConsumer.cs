b973512a25c19d98f733017d3deafed4a3652750
Andershsm <10503096@ucn.dk>
2025-05-26 11:32:03 +0200

    Staged

---
+++ Transform/Kafka/IKafkaConsumer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace Transform.Kafka;
+
+public interface IKafkaConsumer
+{
+    Task ConsumeAsync(CancellationToken cancellationToken, Func<string,Task> onMessageReceivedAsync);
+}
